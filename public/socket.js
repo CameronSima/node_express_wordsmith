6626e5ae30d58b6767e59946daa926581a72398b
Cameron Sima <cjsima@gmail.com>
2015-10-05 16:38:40 -0400

    Animations and multiplayer modal added

--- public/socket.js
+++ public/socket.js
@@ -1,10 +1,25 @@
 window.onload = function () {
     var socket = io.connect('http://localhost:3700');
     var messageBox = document.getElementById('message_box');
+ 
 
     socket.on('message', function (data) {
             messageBox.innerHTML = data.message;
+            if (data.message.substr(data.message.length - 3) == '...') {
+            	(function pulse() {
+            		$(messageBox).delay(200).fadeOut('slow').delay(50).fadeIn('slow', pulse);
+            		if (pulse == false) {
+            			return;
+            		}
+            }
+            )();
+            } else {
+            	$(messageBox).stop();
+            };
 
+
+
+		
     });
 
     socket.on('getLetterSet', function (letters) {
