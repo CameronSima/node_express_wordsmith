4c27d1246c5a141b597cd873e2b505e2eea0bd48
Cameron Sima <cjsima@gmail.com>
2015-10-23 15:03:29 -0400

    bug fixes

--- scripts/botDbEntryScript.js
+++ scripts/botDbEntryScript.js
@@ -1,6 +1,14 @@
 // Script for entering all bots into the db.
-var n = require('../bot/names');
 
+var n = require('../bot/names');
+var User = require('../models/user');
+var bCrypt = require('bcrypt-nodejs');
+
+var createHash = function(password){
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+}
+
+var botEntry = function () {
 for (var i=0; i<=n.botnames.length; i++) {
     var newUser = new User();
     newUser.username = n.botnames[i];
@@ -12,3 +20,6 @@ for (var i=0; i<=n.botnames.length; i++) {
     })
 
 };
+}
+
+botEntry();
